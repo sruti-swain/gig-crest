489c29b6a623ca537b4aafb9a5e73d1371454b26
Sruti-Swain <161316654+sruti-swain@users.noreply.github.com>
2026-04-03 03:32:44 +0530

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-   serverExternalPackages: ['fs', 'path'],
-};
-
-export default nextConfig;
